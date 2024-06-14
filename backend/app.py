d5d34683981de38febe9ff88ee885c934cab4857
MartinR260 <martinkata2006@gmai.com>
2024-06-14 23:57:16 +0300

    Changed Files

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from flask_cors import CORS
+import requests
+
+realm_id = "myrealm"
+client_id = "backend"
+client_secret = "6UegGiQgnZo3pcOyGh0azilWfY1z02Gv"
 
 app = Flask(__name__)
+CORS(app)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/", methods=["GET", "POST"])
+def validate_token():
+    data = request.json
+    response = requests.post(f"http://localhost:8080/realms/{realm_id}/protocol/openid-connect/token/introspect", {"client_id": client_id, "client_secret": client_secret, "token": data["token"]}).json()
+    return make_response(response, 200)
+
+if __name__ == "__main__":
+    app.run(port=5001, host="0.0.0.0", debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Keycloak from "keycloak-js";
+
+let initOptions = {
+  url: 'http://localhost:8080/',
+  realm: 'myrealm',
+  clientId: 'frontend',
+}
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required', // Supported values: 'check-sso' (default), 'login-required'
+  checkLoginIframe: true
+}).then((auth) => {
+  if (auth) {
+    window.location.reload();
+  }
+  else {
+    alert("Authenticated");
+    alert(auth)
+    alert(kc)
+    console.log(kc.token)
+
+    httpClient.defaults.headers.common['Authorization'] = `Bearer ${kc.token}`;
+    sessionStorage.setItem("token", kc.token);
+
+    kc.onTokenExpired = () => {
+      alert('token expired')
+    }
+  }
+}, () => {
+  alert("Authentication Failed");
+});
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editiwritethistoseeifitwillchange <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +51,9 @@ function App() {
         >
           Learn React
         </a>
+        <br></br>
+        <button onClick={() => {kc.login();}}>Login</button>
+        <button onClick={() => {sessionStorage.clear();kc.logout();}}>Logout</button>
       </header>
     </div>
   );
